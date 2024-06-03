d912d6d30b1f59f42e1ce395f6905816f5e5fd32
Ivan <apedoivan@gmail.com>
2024-06-03 06:38:39 +0200

    some improvments

---
+++ README.md
@@ -0,0 +1,12 @@
+Huffman compresser implementation in Go
+
+
+### Compression
+````
+go run main.go -compress -input input.txt -output output.bin 
+````
+
+### deCompression
+````
+go run main.go -decompress -input output.bin -output output.txt 
+````
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"sort"
+	"strings"
 )
 
 // Define a struct for the Huffman tree node
@@ -67,11 +70,11 @@ func generateCodeRec(node *Node, code string, codes HuffmanCode) {
 
 // Function to encode data using Huffman codes
 func encode(data []byte, codes HuffmanCode) string {
-	encoded := ""
+	var encoded strings.Builder
 	for _, symbol := range data {
-		encoded += codes[symbol]
+		encoded.WriteString(codes[symbol])
 	}
-	return encoded
+	return encoded.String()
 }
 
 // Function to write encoded data to file
@@ -87,49 +90,145 @@ func writeEncodedData(encodedData string, codes HuffmanCode, outputFileName stri
 		fmt.Fprintf(outputFile, "%c:%s\n", symbol, code)
 	}
 
+	// Separator between codes and encoded data
+	fmt.Fprintln(outputFile, "DATA")
+
 	// Write encoded data to file
 	_, err = outputFile.WriteString(encodedData)
 	return err
 }
 
+// Function to read Huffman codes from file
+func readCodes(scanner *bufio.Scanner) (HuffmanCode, error) {
+	codes := make(HuffmanCode)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "DATA" {
+			break
+		}
+		if len(line) > 2 && line[1] == ':' {
+			codes[line[0]] = line[2:]
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return codes, nil
+}
+
+// Function to decode data using Huffman codes
+func decode(encodedData string, codes HuffmanCode) []byte {
+	reverseCodes := make(map[string]byte)
+	for symbol, code := range codes {
+		reverseCodes[code] = symbol
+	}
+	var decoded strings.Builder
+	code := ""
+	for _, bit := range encodedData {
+		code += string(bit)
+		if symbol, ok := reverseCodes[code]; ok {
+			decoded.WriteByte(symbol)
+			code = ""
+		}
+	}
+	return []byte(decoded.String())
+}
+
+// Function to read encoded data from file
+func readEncodedData(scanner *bufio.Scanner) (string, error) {
+	var encodedData strings.Builder
+	for scanner.Scan() {
+		line := scanner.Text()
+		encodedData.WriteString(line)
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	return encodedData.String(), nil
+}
+
 func main() {
-	// Read input data
-	inputFileName := "input.txt"
-	inputFile, err := os.Open(inputFileName)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	compress := flag.Bool("compress", false, "Compress the input file")
+	decompress := flag.Bool("decompress", false, "Decompress the input file")
+	inputFileName := flag.String("input", "input.txt", "Input file name")
+	outputFileName := flag.String("output", "output.bin", "Output file name")
+	flag.Parse()
+
+	if *compress {
+		// Read input data
+		inputFile, err := os.Open(*inputFileName)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer inputFile.Close()
+
+		data, err := io.ReadAll(inputFile)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Calculate symbol frequencies
+		frequencies := make(map[byte]int)
+		for _, symbol := range data {
+			frequencies[symbol]++
+		}
+
+		// Build Huffman tree
+		root := buildHuffmanTree(frequencies)
+
+		// Generate Huffman codes
+		codes := generateCodes(root)
+
+		// Encode input data
+		encodedData := encode(data, codes)
+
+		// Write encoded data to output file
+		err = writeEncodedData(encodedData, codes, *outputFileName)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println("Compression successful. Output written to", *outputFileName)
+	} else if *decompress {
+		// Read input file
+		inputFile, err := os.Open(*inputFileName)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer inputFile.Close()
+
+		scanner := bufio.NewScanner(inputFile)
+
+		// Read Huffman codes
+		codes, err := readCodes(scanner)
+		if err != nil {
+			fmt.Println("Error reading Huffman codes:", err)
+			return
+		}
+
+		// Read encoded data
+		encodedData, err := readEncodedData(scanner)
+		if err != nil {
+			fmt.Println("Error reading encoded data:", err)
+			return
+		}
+
+		// Decode data
+		decodedData := decode(encodedData, codes)
+
+		// Write decoded data to output file
+		err = os.WriteFile(*outputFileName, decodedData, 0644)
+		if err != nil {
+			fmt.Println("Error writing decoded data:", err)
+			return
+		}
+
+		fmt.Println("Decompression successful. Output written to", *outputFileName)
+	} else {
+		fmt.Println("Please specify either -compress or -decompress.")
 	}
-	defer inputFile.Close()
-
-	data, err := io.ReadAll(inputFile)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Calculate symbol frequencies
-	frequencies := make(map[byte]int)
-	for _, symbol := range data {
-		frequencies[symbol]++
-	}
-
-	// Build Huffman tree
-	root := buildHuffmanTree(frequencies)
-
-	// Generate Huffman codes
-	codes := generateCodes(root)
-
-	// Encode input data
-	encodedData := encode(data, codes)
-
-	// Write encoded data to output file
-	outputFileName := "compressed.bin"
-	err = writeEncodedData(encodedData, codes, outputFileName)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	fmt.Println("Compression successful. Output written to", outputFileName)
 }
